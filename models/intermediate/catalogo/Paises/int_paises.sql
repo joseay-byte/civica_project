ba986cee01f421e8d01bcfb0e9611142fe7680ff
jose ayrton rosell bonavina <joseayrtonrosellbonavina@gmail.com>
2026-05-19 19:41:56 +0000

    Models de la capa gold y tests hechos

--- models/intermediate/catalogo/Paises/int_paises.sql
+++ models/intermediate/catalogo/Paises/int_paises.sql
@@ -5,17 +5,18 @@ WITH seed_paises AS (
 ),
 paises_agrupados AS (
     SELECT 
-        TRIM(country_name) AS nombre_pais,
+        TRIM(country_name) AS nombre_pais_EN,
+        TRIM(nombre_pais_es) AS nombre_pais,
         MAX(UPPER(TRIM(iso_2))) AS codigo_pais,
         MAX(TRIM(continent)) AS continente,
         MAX(UPPER(TRIM(iso_3))) AS codigo_iso3,
         MAX(is_axis) AS is_axis,
         MAX(is_allied) AS is_allied
     FROM seed_paises
-    GROUP BY TRIM(country_name)
+    GROUP BY TRIM(country_name), TRIM(nombre_pais_es)
 )
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['UPPER(nombre_pais)']) }} AS id_pais,
+    {{ dbt_utils.generate_surrogate_key(['UPPER(nombre_pais_EN)']) }} AS id_pais,
     nombre_pais,
     codigo_pais,
     continente,
